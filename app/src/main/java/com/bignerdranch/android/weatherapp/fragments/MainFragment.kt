18fee807776657ba08a1aba3d9e5f3256a6546bc
IES19991021 <erina.1r@yandex.ru>
2023-06-11 23:44:02 +0700

    Шаг 7. Подключили TabLayout и ViewPager2. Создали ViewPager Adapter для переключения между вкладками

--- app/src/main/java/com/bignerdranch/android/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/weatherapp/fragments/MainFragment.kt
@@ -6,14 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.bignerdranch.android.weatherapp.R
+import com.bignerdranch.android.weatherapp.adapters.VpAdapter
 import com.bignerdranch.android.weatherapp.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
     private lateinit var plauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -29,6 +41,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout2, vp){
+            tab, post -> tab.text = tList[post]
+        }.attach()
     }
 
 
