b4346d9ff931024e1a31e42c129d7776344c1be6
IES19991021 <erina.1r@yandex.ru>
2023-06-11 21:01:33 +0700

    Шаг 5. Добавили проверку на разрешение местоположения

---
+++ app/src/main/java/com/bignerdranch/android/weatherapp/fragments/Extensions.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.weatherapp.fragments
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.isPermissionGranted(p: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        activity as AppCompatActivity, p) == PackageManager.PERMISSION_GRANTED
+
+}
