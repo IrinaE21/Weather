f31dc380a07f13ded413151802ae2a93e74136cb
IES19991021 <erina.1r@yandex.ru>
2023-06-12 21:56:19 +0700

    Шаг 12. Парсинг полученных данных с API, создали функцию parseWeatherData

--- app/src/main/java/com/bignerdranch/android/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/weatherapp/fragments/MainFragment.kt
@@ -17,8 +17,10 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bignerdranch.android.weatherapp.R
 import com.bignerdranch.android.weatherapp.adapters.VpAdapter
+import com.bignerdranch.android.weatherapp.adapters.WeatherModel
 import com.bignerdranch.android.weatherapp.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import org.json.JSONObject
 
 const val API_KEY = "c226fc1e5e3344bea1e174450230906"
 
@@ -89,7 +91,7 @@ class MainFragment : Fragment() {
             Request.Method.GET,
             url,
             {
-                result -> Log.d("MyLog", "Result: $result")
+                result -> parseWeatherData(result)
             },
             {
                 error -> Log.d("MyLog", "Error: $error")
@@ -98,6 +100,26 @@ class MainFragment : Fragment() {
 
         )
         queue.add(request)
+    }
+
+    private fun parseWeatherData(result: String){
+        val mainObject = JSONObject(result)
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            "",
+            "",
+            mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
+            ""
+        )
+        Log.d("MyLog", "City: ${item.city}")
+        Log.d("MyLog", "Time: ${item.time}")
+        Log.d("MyLog", "Condition: ${item.condition}")
+        Log.d("MyLog", "Temp: ${item.currentTemp}")
+        Log.d("MyLog", "Url: ${item.imageUrl}")
+
     }
 
     companion object {
