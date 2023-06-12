4b076780b627e348baf782e3ea40568082f3684f
IES19991021 <erina.1r@yandex.ru>
2023-06-12 18:13:51 +0700

    Шаг 8. Cоздали data class WeatherModel и разметку элемента для списка list_item.xml.

--- app/src/main/java/com/bignerdranch/android/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/weatherapp/MainViewModel.kt
@@ -2,11 +2,12 @@ package com.bignerdranch.android.weatherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.weatherapp.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
     //храним и обновляем информацию для карточки(главное окно)
-    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
     //храним список с погнозом погоды на ближащие дни
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 
 }
